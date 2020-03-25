d2c0bf8ed9d554f7a984da81cefadaafd7eb313e
atakan <atakanhr@gmail.com>
2020-03-26 02:37:35 +0300

    v 1.0

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Test Title</title>
+</head>
+<link rel="stylesheet" href="/css/main.css">
+<script src="https://cdn.jsdelivr.net/npm/places.js@1.18.1"></script>
+<script src="/js/skycons.js"></script>
+<script src="/js/app.js" defer></script>
+
+<body class="bg-blue-500">
+    <div id="app" class="flex justify-center pt-16">
+        <Weather-app></Weather-app>
+
+
+    </div>
+
+</body>
+</html>
