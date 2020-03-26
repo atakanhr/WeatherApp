c816095c375c55fbedf54e40a033afecebb5936f
atakan <atakanhr@gmail.com>
2020-03-26 03:01:51 +0300

    v1.0.1
    readme

--- public/js/app.js
+++ public/js/app.js
@@ -49884,8 +49884,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\Codes\Weather-App\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\Codes\Weather-App\resources\css\main.css */"./resources/css/main.css");
+__webpack_require__(/*! D:\codes\Weather-App\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\codes\Weather-App\resources\css\main.css */"./resources/css/main.css");
 
 
 /***/ })
